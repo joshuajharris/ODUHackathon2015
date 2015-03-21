b31833db50b4e0134aa0fb0f368680a4a2e72f57
Joshua Harris <joshua.jharris13@gmail.com>
2015-03-21 19:28:08 -0400

    Improved profile functionality, introduced states for view changing

--- profileCont.js
+++ profileCont.js
@@ -1,15 +1,34 @@
 if(Meteor.isClient) {
+
+  Template.profile.helpers({
+    fname: function(){ return Session.get('fname') }
+  });
+
   Template.profile.events({
     'submit .edit-profile': function(event){
       event.preventDefault();
 
       var email = event.target.email.value,
+          pass = event.target.pass.value,
+          passConfirm = event.target.passConfirm.value,
           first = event.target.first.value,
           last = event.target.last.value;
-
+/*
+      if(pass == passConfirm){
+        Meteor.users.update({_id:Meteor.user()._id},{$set:{"password":pass}});
+        event.target.pass.value = "";
+        event.target.passConfirm.value= "";
+      }
+*/
       if(first) {
+        Session.set('fname', first);
         Meteor.users.update({_id:Meteor.user()._id},{$set:{"profile.fname":first}});
         event.target.first.value = "";
+      }
+
+      if(last) {
+        Meteor.users.update({_id:Meteor.user()._id},{$set:{"profile.lname":last}});
+        event.target.last.value = "";
       } 
     }
   });
--- survey-app.js
+++ survey-app.js
@@ -1,5 +1,16 @@
 if (Meteor.isClient) {
+  Session.setDefault('state', 'register');
 
+  Template.body.helpers({
+    state: function(){ return Session.get('state') },
+    isProfileState: function(){ return Session.get('state') == 'profile'}           
+  });
+
+  Template.body.events({
+    'click .profileLink': function(event) {
+      Session.set('state', 'profile'); 
+    }
+  });
 }
 
 if (Meteor.isServer) {
