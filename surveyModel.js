8813af58267f08fb469b3c64aed06851b2e74e79
washt <washt23@gmail.com>
2015-03-21 17:20:27 -0400

    updated surveyModel schema

--- surveyModel.js
+++ surveyModel.js
@@ -1,10 +1,26 @@
 if(Meteor.isClient) {
 
-Session.setDefault('question', "");
+Survey = new SimpleSchema({
+  question: {
+    type: String,
+    max: 100
+  },
+  response: {
+    type: String,
+    max: 50
+  },
+  duration: {
+    type: Number,
+    max: 60
+  },
+  description: {
+    type: String,
+    max: 250
+  },
+  weight : {
+  	type: Number,
+  	max: 10
+  }
 
-	Template.survey.helpers({
-		questions : [{"Who are You": ""},{"What Are You":""},{"What":""}]
-	});
-
-	Template.surver.events({});
+});
 }
