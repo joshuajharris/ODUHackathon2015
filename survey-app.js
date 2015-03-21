dd8aeaa51a224177449392f3a65b52fc38ccc6ea
Joshua Harris <joshua.jharris13@gmail.com>
2015-03-21 19:57:28 -0400

    Misc

--- survey-app.js
+++ survey-app.js
@@ -1,11 +1,11 @@
 if (Meteor.isClient) {
   Session.setDefault('state', 'register');
-
+  // allow body to get state
   Template.body.helpers({
     state: function(){ return Session.get('state') },
     isProfileState: function(){ return Session.get('state') == 'profile'}           
   });
-
+  // add an event listener to profile link to change state
   Template.body.events({
     'click .profileLink': function(event) {
       Session.set('state', 'profile'); 
