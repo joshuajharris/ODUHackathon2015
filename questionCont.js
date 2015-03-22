443e22e9994c86fecc089d58680b5f6e39bd43e3
Joshua Harris <joshua.jharris13@gmail.com>
2015-03-21 22:55:03 -0400

    Misc

--- questionCont.js
+++ questionCont.js
@@ -6,4 +6,13 @@ if(Meteor.isClient){
     questions: function(){ return Questions.find({})}
   }); 
 
+  Questions.insert({question: "How much foo do you bar?", id: 0});
+  Questions.insert({question: "How bar do you foo?", id: 1});
+  Questions.insert({question: "Do you like foobar?", id: 2});
+  Questions.insert({question: "Can you foobar?", id: 3});
+
+
+  Questions.find({}).forEach(function(q){
+    //Meteor.users.update({_id:Meteor.user()._id},{$set:{"profile.ans":{"id":q.id, "answered": false}}});
+  });
 }
